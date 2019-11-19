41cd37e99246ee5f49dc9d7c76e20ea7b6ce0d84
tihonov.AN <tihonov.an@lsrgroup.ru>
2019-11-19 16:52:38 +0300

    Камень судьбы

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+require_once 'src/classes/Task.php';
+
+use app\Tasks\Task;
+
+$newTask = new Task();
+
+assert($newTask->getNewStatus(Task::ACTION_NEW) === Task::STATUS_NEW, 'статус "New" не присвоен');
+assert($newTask->getNewStatus(Task::ACTION_START) === Task::STATUS_EXECUTION, 'статус "Execution" не присвоен');
+assert($newTask->getNewStatus(Task::ACTION_CANCEL) === Task::STATUS_CANCELED, 'статус "Cancel" не присвоен');
+assert($newTask->getNewStatus(Task::ACTION_REFUSE) === Task::STATUS_FAILED, 'статус "Failed" не присвоен');
+assert($newTask->getNewStatus(Task::ACTION_COMPLETE) === Task::STATUS_DONE, 'статус "Done" не присвоен');
