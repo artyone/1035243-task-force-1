568fd910b0e4a429fc317b737540687bf3f167df
tihonov.AN <tihonov.an@lsrgroup.ru>
2020-02-28 11:40:07 +0300

    Задания и исполнители: изменение helpers 2

--- frontend/helpers/Pluralize.php
+++ frontend/helpers/Pluralize.php
@@ -3,8 +3,9 @@
 
 namespace frontend\helpers;
 
+
 /**
- * Класс-помощник получения склонения слов в зависимости от числительного перед ними
+ * Класс-помощник для получения склонений существительных в зависимости от числительного перед ними
  * Class Pluralize
  * @package frontend\helpers
  */
@@ -15,88 +16,96 @@ class Pluralize
     const SECONDS_IN_MINUTE = 60;
 
     /**
+     * @var array массив склонений необходимых слов для числительных (1, 4, 5)
+     */
+    private $wordDeclination = [
+        'minutes' => [' минуту', ' минуты', ' минут'],
+        'hours' => [' чаc', ' часа', ' часов'],
+        'days' => [' день', ' дня', ' дней'],
+        'feedbacks' => [' отзыв', ' отзыва', ' отзывов'],
+        'tasks' => [' задание', ' задания', ' заданий']
+    ];
+
+    /**
+     * Метод получения параметров для подсчета склонения существительных связанных со временем
+     * @param string|null $time
+     * @return array
+     */
+    public function getParamTime(?string $time): array
+    {
+        $seconds = strtotime("now") - strtotime($time);
+        $minutes = $seconds / self::SECONDS_IN_MINUTE;
+        if ($minutes < self::MINUTES_IN_HOUR) {
+            return [$minutes, 'minutes'];
+        }
+        $hours = $minutes / self::MINUTES_IN_HOUR;
+        if ($hours < self::HOURS_IN_DAY) {
+            return [$hours, 'hours'];
+        }
+        $days = $hours / self::HOURS_IN_DAY;
+        return [$days, 'days'];
+    }
+    /**
      * Метод вычисления склонения слова в зависимости от числительного перед ним
      * @param int|null $number число перед словом
-     * @param array $declensions массив склонений слова для числительных (1, 4, 5)
      * @return string
      */
-    function getWordPluralize(?int $number, array $declensions): string
+    public function getWordPluralize(?int $number, string $word): string
     {
 
         if ($number % 100 >= 11 && $number % 100 <= 19) {
-            $string = $declensions[2];
+            $string = $this->wordDeclination[$word][2];
         } else {
             switch ($number % 10) {
                 case (1):
-                    $string = $declensions[0];
+                    $string = $this->wordDeclination[$word][0];
                     break;
                 case (2):
                 case (3):
                 case (4):
-                    $string = $declensions[1];
+                    $string = $this->wordDeclination[$word][1];
                     break;
                 default:
-                    $string = $declensions[2];
+                    $string = $this->wordDeclination[$word][2];
             }
         }
         return $number . " $string";
     }
 
     /**
-     * Метод для получения правильного склонения слов "минута", "час" и "день" в зависимости от количества времени
+     * Метод для получения правильного склонения существительных "минута", "час" и "день" в зависимости от количества времени
      * @param string|null $time строка времени
      * @return string количество времени и подходящее слово с правильным склонением
      */
     static function getStringTimeAgo(?string $time): string
     {
-        $wordDeclinationMinutes = [' минуту', ' минуты', ' минут'];
-        $wordDeclinationHours = [' чаc', ' часа', ' часов'];
-        $wordDeclinationDays = [' день', ' дня', ' дней'];
-
-        $seconds = strtotime("now") - strtotime($time);
-        $minutes = $seconds / Pluralize::SECONDS_IN_MINUTE;
-        if ($minutes < Pluralize::MINUTES_IN_HOUR) {
-            $string = new Pluralize();
-            $result = $string->getWordPluralize((int)$minutes, $wordDeclinationMinutes);
-            return $result;
-        }
-        $hours = $minutes / Pluralize::MINUTES_IN_HOUR;
-        if ($hours < Pluralize::HOURS_IN_DAY) {
-            $string = new Pluralize();
-            $result = $string->getWordPluralize((int)$hours, $wordDeclinationHours);
-            return $result;
-        }
-        $days = $hours / Pluralize::HOURS_IN_DAY;
         $string = new Pluralize();
-        $result = $string->getWordPluralize((int)$days, $wordDeclinationDays);
-
+        $param = $string->getParamTime($time);
+        $result = $string->getWordPluralize((int)$param[0], $param[1]);
         return $result;
-
     }
 
     /**
-     * Метод для получения правильного склонения слова "отзыв" в зависимости от числительного перед ним.
+     * Метод для получения правильного склонения существительного "отзыв" в зависимости от числительного перед ним.
      * @param int|null $number число отзывов
      * @return string количество заданий и существительное в правильном склонении
      */
     static function getStringFeedbacks(?int $number): string
     {
-        $wordDeclination = [' отзыв', ' отзыва', ' отзывов'];
         $string = new Pluralize();
-        $result = $string->getWordPluralize($number, $wordDeclination);
+        $result = $string->getWordPluralize($number, 'feedbacks');
         return $result;
     }
 
     /**
-     * Метод для получения правильного склонения слова "задание" в зависимости от числительного перед ним.
+     * Метод для получения правильного склонения существительного "задание" в зависимости от числительного перед ним.
      * @param int|null $number число заданий
      * @return string количество заданий и существительное в правильном склонении
      */
     static function getStringTasks(?int $number): string
     {
-        $wordDeclination = [' задание', ' задания', ' заданий'];
         $string = new Pluralize();
-        $result = $string->getWordPluralize($number, $wordDeclination);
+        $result = $string->getWordPluralize($number, 'tasks');
         return $result;
     }
 }
