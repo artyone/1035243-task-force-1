e2f5ebf474435cdca0e46a716d3da9f6de47089b
tihonov.AN <tihonov.an@lsrgroup.ru>
2020-03-05 13:24:42 +0300

    Тайные тропы: реворк конвертера времени

--- frontend/web/test.php
+++ frontend/web/test.php
@@ -1,6 +1,6 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
+/*ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
@@ -31,9 +31,22 @@ try {
     print $exception->getMessage() . "\n";
 } catch (StatusException $exception){
     print $exception->getMessage() . "\n";
+}*/
+
+function ago($datetime)
+{
+    $interval = date_create('now')->diff( date_create($datetime));
+/*    $suffix = ( $interval->invert ? ' ago' : '' );
+    if ( $v = $interval->y >= 1 ) return pluralize( $interval->y, 'year' ) . $suffix;
+    if ( $v = $interval->m >= 1 ) return pluralize( $interval->m, 'month' ) . $suffix;
+    if ( $v = $interval->d >= 1 ) return pluralize( $interval->d, 'day' ) . $suffix;
+    if ( $v = $interval->h >= 1 ) return pluralize( $interval->h, 'hour' ) . $suffix;
+    if ( $v = $interval->i >= 1 ) return pluralize( $interval->i, 'minute' ) . $suffix;
+    return pluralize( $interval->s, 'second' ) . $suffix;*/
+    return $interval->y;
 }
 
-
+echo ago('2020-11-13 00:00:00');
 
 
 
